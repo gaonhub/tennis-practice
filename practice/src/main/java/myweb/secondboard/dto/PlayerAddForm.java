85ae199fea0fa321cf8c9fa97af7fbe1669d785b
rkdhs321 <rkdhs321@gmail.com>
2022-11-03 14:42:50 +0900

    .

--- practice/src/main/java/myweb/secondboard/dto/PlayerAddForm.java
+++ practice/src/main/java/myweb/secondboard/dto/PlayerAddForm.java
@@ -3,10 +3,13 @@ package myweb.secondboard.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter @Setter
 public class PlayerAddForm {
 
     String memberId;
+    @NotNull
     String team;
     String matchId;
 }
